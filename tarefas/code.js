a3ef540fcb0caf7f16c2248cb5bccd3d2de51da8
Natan Alexandre <natanalexandre742@gmail.com>
2023-08-09 11:58:29 -0300

    Update

---
+++ tarefas/code.js
@@ -0,0 +1,16 @@
+function AdicionarLinha(){
+    var tabela = window.document.querySelector('tbody')
+    var linha = tabela.insertRow(tabela.rows.length)
+    var tarefa = linha.insertCell(0)
+    var feito = linha.insertCell(1)
+    var data = linha.insertCell(2)
+    
+    tarefa.innerHTML = '<input type="text" class="tarefa_input">'
+    feito.innerHTML = '<input type="checkbox" name="feito">'
+    data.innerHTML = '<input type="date">'
+}
+
+function ApagarLinha(){
+    var tabela = window.document.querySelector('tbody')
+    var linha = tabela.deleteRow(tabela.rows)
+}
