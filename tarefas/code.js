4e4aee960e39c86fb073b8f8365a2062964a502c
Natan Alexandre <natanalexandre742@gmail.com>
2023-08-09 13:30:24 -0300

    Update

--- tarefas/code.js
+++ tarefas/code.js
@@ -3,11 +3,9 @@ function AdicionarLinha(){
     var linha = tabela.insertRow(tabela.rows.length)
     var tarefa = linha.insertCell(0)
     var feito = linha.insertCell(1)
-    var data = linha.insertCell(2)
     
     tarefa.innerHTML = '<input type="text" class="tarefa_input" placeholder="Digite sua tarefa">'
     feito.innerHTML = '<input type="checkbox" name="feito">'
-    data.innerHTML = '<input type="date">'
 }
 
 function ApagarLinha(){
