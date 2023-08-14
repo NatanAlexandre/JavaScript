763ac6f586360c1c1fd970b945d0a9eb87b76a9a
Natan Alexandre <natanalexandre742@gmail.com>
2023-08-14 17:47:41 -0300

    Update

--- horario/code.js
+++ horario/code.js
@@ -5,16 +5,19 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12){
         // Bom dia!
         img.src = 'fotomanha.png'
+        document.body.style.background = "#e2cd9f"
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde!
         img.src = 'fototarde.png'
+        document.body.style.background = "#b9846f"
     } else {
         // Boa noite!
         img.src = 'fotonoite.png'
+        document.body.style.background = "#515154"
     }
 }
